4aa206f38d146fbff587e010bc14912123c3469d
stefan <stefan@omszone.com>
2018-09-07 16:35:54 +0200

    refactor re-usable components and make them available throughout the app via single import only

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Header from './components/common/Header';
+import { View, Text, StyleSheet } from 'react-native';
+import { Header } from './components/common/';
 
 
 class App extends Component {
     render() {
         return(
-            <View>
-                <Header>
-                    <Text headerText="">
-                        An App
-                    </Text>
-                </Header>
+            <View style={styles.container}>
+                <Header headerText="Authentication" />
+                <Text>An app</Text>
             </View>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
 
 export default App;
